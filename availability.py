a0a05b8a37e2036b4b02720da385ddc8558396a1
Dejoe John <22582871+dej0e@users.noreply.github.com>
2021-05-02 13:03:37 +0530

    Update availability.py

--- availability.py
+++ availability.py
@@ -62,7 +62,7 @@ def get_availability(days: int, district_ids: List[int], min_age_limit: int):
                 all_date_df = df
 
     if all_date_df is not None:
-        all_date_df = all_date_df.drop(["block_name"], axis=1).sort_values(["min_age_limit", "state_name", "district_name", "available_capacity"], ascending=[True, True, True, False])
+        all_date_df = all_date_df.drop(["block_name"], axis=1).sort_values(["min_age_limit", "state_name", "district_name", "available_capacity"], ascending=[True, False, True, False])
         return all_date_df[all_date_df.min_age_limit >= min_age_limit]
     return pd.DataFrame()
 
