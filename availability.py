132fd5b7564b3ca2e355d298eb36e377d644f1ab
Dejoe John <22582871+dej0e@users.noreply.github.com>
2021-05-02 12:48:11 +0530

    Remove testing code.

--- availability.py
+++ availability.py
@@ -46,7 +46,6 @@ def get_availability(days: int, district_ids: List[int], min_age_limit: int):
     all_date_df = None
 
     for district_id in district_ids:
-        sleep(3)
         print(f"checking for INP_DATE:{INP_DATE} & DIST_ID:{district_id}")
         URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(district_id, INP_DATE)
         print(f"{URL}")
